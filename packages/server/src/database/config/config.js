668a57e814f74d87428d79d996267b1834735b83
lgants <lgants@gmail.com>
2018-06-26 22:01:53 -0500

    no longer getting an error when connecting

--- packages/server/src/database/config/config.js
+++ packages/server/src/database/config/config.js
@@ -1,32 +1,9 @@
-var settings = require('../../../../../settings');
-// ver fs = require('fs');
-
 module.exports = {
   development: {
     username: 'root',
-    password: null,
-    database: 'dev-db',
+    password: 'root',
+    database: 'articles',
     host: 'localhost',
-    pool: settings.db.pool,
-    dialect: settings.db.dialect,
-    storage: settings.db.connection.development.filename
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'test-db',
-    host: 'localhost',
-    pool: settings.db.pool,
-    dialect: settings.db.dialect,
-    storage: settings.db.connection.development.filename
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'prod-db',
-    host: 'localhost',
-    pool: settings.db.pool,
-    dialect: settings.db.dialect,
-    storage: ':memory:'
+    dialect: 'mysql'
   }
 };
